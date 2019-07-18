0be86f0d1df7ce5e3e1c43f26922870e1264ea99
Baffoe123 <52656106+Baffoe123@users.noreply.github.com>
2019-07-18 12:17:53 +0000

    Update mywebapp.py

--- mywebapp.py
+++ mywebapp.py
@@ -21,5 +21,7 @@ def whereami():
     return 'Ghana!'
  
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = os.environ.get('PORT', 5000)
+    app.run(debug=True, host='0.0.0.0',port=port)
+
 
